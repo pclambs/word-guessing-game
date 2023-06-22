7b3b2eb3e75f182872dd85b5f6f633df65d3bb24
deepwubs <pchucklambs@gmail.com>
2023-06-22 15:56:26 -0500

    moved local files over to repo

---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+//vars
+//  select dom elements\
+  // start button
+  // reset button
+  // count down timer
+  // word-blanks
+  // win counter
+  // loses
+
+// list of words
+// userGuesses = []     list of letters guessed by user
+// 
+function startRound() {
+  // start timer
+  // pick a random word
+  // set word-blanks to have ___'s
+}
+
+function onKeydown(event) {
+  // check if letter pressed is in the answer string
+    // if yes
+      // reveal letter
+}
+
+function tick() {
+  // check if userGuesses has all tletters that are in answer string
+    // if has guess all letters, 
+      // end round
+      // increment wins
+      // display you won
+    // else 
+      // is timer count at 0
+        // if yes
+          // display round lost
+          // increment loses
+      // else
+        // decrement timer count
+}
+// event listeners
+  // start
+  // reset
+  // keydown onKeydown
+
+
+//
+// 
+// 
+// 
+//
