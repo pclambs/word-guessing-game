c34d3befd47d603e3192511c35a16012fe03a12d
deepwubs <pchucklambs@gmail.com>
2023-06-22 16:16:19 -0500

    vars and start round listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,19 +7,14 @@ var wordBlanksEl = document.querySelector(".word-blanks")
 var winsEl = document.querySelector(".win")
 var lossesEl = document.querySelector(".lose")
 
-console.log(startButton, resetButton, timerEL,wordBlanksEl, winsEl,lossesEl)
-
-  // count down timer
-  // word-blanks
-  // win counter
-  // loses
-
 // list of words
-// userGuesses = []     list of letters guessed by user
-// 
+var words = ["bacon", "pancakes", "waffels", "eggs", "cheese"]
+var userGuesses = [] //list of letters guessed by user
+var answer
+
 function startRound() {
   // start timer
-  // pick a random word
+  console.log("starting Round")  // pick a random word
   // set word-blanks to have ___'s
 }
 
@@ -45,12 +40,6 @@ function tick() {
 }
 // event listeners
   // start
+startButton.addEventListener("click", startRound)
   // reset
   // keydown onKeydown
-
-
-//
-// 
-// 
-// 
-//
